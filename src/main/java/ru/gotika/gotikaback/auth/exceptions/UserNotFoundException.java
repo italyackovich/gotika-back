17c16bc0152a335d386a05feebdf18b550635b51
Ilya <pechenka.ivk3@gmail.com>
2025-01-24 14:12:28 +0400

    fix: created UserNotFoundException to catch errors related to not found user

---
+++ src/main/java/ru/gotika/gotikaback/auth/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.gotika.gotikaback.auth.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(final String message) {
+        super(message);
+    }
+}
