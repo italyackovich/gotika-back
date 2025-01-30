849c47ae264509177bf6f8918ab26c0b66f14f4a
Ilya <pechenka.ivk3@gmail.com>
2025-01-30 21:20:31 +0400

    refactor: removed unused imports

--- src/main/java/ru/gotika/gotikaback/auth/controller/AuthController.java
+++ src/main/java/ru/gotika/gotikaback/auth/controller/AuthController.java
@@ -4,9 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import lombok.RequiredArgsConstructor;
 import org.apache.hc.core5.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.*;
 import ru.gotika.gotikaback.auth.dto.*;
 import ru.gotika.gotikaback.auth.service.AuthService;
