8a30deef115966d7e32fe66229eacc1d00dc258e
Ilya <pechenka.ivk3@gmail.com>
2025-02-03 19:09:43 +0400

    feat: added validation for AuthRequest class

--- src/main/java/ru/gotika/gotikaback/auth/dto/AuthRequest.java
+++ src/main/java/ru/gotika/gotikaback/auth/dto/AuthRequest.java
@@ -1,5 +1,6 @@
 package ru.gotika.gotikaback.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AuthRequest {
 
+    @NotBlank(message = "Email is required")
     private String email;
+
+    @NotBlank(message = "Password is required")
     private String password;
 }
