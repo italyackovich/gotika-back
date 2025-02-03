fd21d716088835e5b64affa13f8e27375733cc36
Ilya <pechenka.ivk3@gmail.com>
2025-02-03 19:49:56 +0400

    fix: added exception handler for validation errors to return detailed error messages

--- src/main/java/ru/gotika/gotikaback/auth/exceptions/AuthExceptionHandler.java
+++ src/main/java/ru/gotika/gotikaback/auth/exceptions/AuthExceptionHandler.java
@@ -4,11 +4,27 @@ import io.jsonwebtoken.ExpiredJwtException;
 import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class AuthExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        error -> Optional.ofNullable(error.getDefaultMessage()).orElse("No error message provided")
+                ));
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException e) {
