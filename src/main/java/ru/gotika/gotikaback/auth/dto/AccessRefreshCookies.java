9d6efede6b99d0cca29f6d94ec51254ac11f39ed
Ilya <pechenka.ivk3@gmail.com>
2025-02-03 19:08:08 +0400

    feat: added validation for AccessRefreshCookies class

--- src/main/java/ru/gotika/gotikaback/auth/dto/AccessRefreshCookies.java
+++ src/main/java/ru/gotika/gotikaback/auth/dto/AccessRefreshCookies.java
@@ -1,5 +1,6 @@
 package ru.gotika.gotikaback.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.ResponseCookie;
@@ -7,6 +8,9 @@ import org.springframework.http.ResponseCookie;
 @Data
 @AllArgsConstructor
 public class AccessRefreshCookies {
+    @NotNull(message = "AccessTokenCookie is required")
     ResponseCookie accessTokenCookie;
+
+    @NotNull(message = "RefreshTokenCookie is required")
     ResponseCookie refreshTokenCookie;
 }
