213b75b641ace45bd583df9cddaed9097b62888a
Ilya <pechenka.ivk3@gmail.com>
2025-02-03 19:52:30 +0400

    chore: moved handle MethodArgumentNotValidException from AuthExceptionHandler to GlobalExceptionHandler

--- src/main/java/ru/gotika/gotikaback/common/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ru/gotika/gotikaback/common/exceptions/GlobalExceptionHandler.java
@@ -2,12 +2,28 @@ package ru.gotika.gotikaback.common.exceptions;
 
 import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.gotika.gotikaback.user.exceptions.UserNotFoundException;
 
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        error -> Optional.ofNullable(error.getDefaultMessage()).orElse("No error message provided")
+                ));
+        return ResponseEntity.badRequest().body(errors);
+    }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
