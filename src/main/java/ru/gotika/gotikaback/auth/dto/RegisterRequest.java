636adcf2017ad57e6799087abe957b4f2d6cfdfd
Ilya <pechenka.ivk3@gmail.com>
2025-02-03 20:03:31 +0400

    feat: added validation for RegisterRequest class

--- src/main/java/ru/gotika/gotikaback/auth/dto/RegisterRequest.java
+++ src/main/java/ru/gotika/gotikaback/auth/dto/RegisterRequest.java
@@ -1,5 +1,8 @@
 package ru.gotika.gotikaback.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotBlank(message = "firstName cannot be blank")
     private String firstName;
+
+    @NotBlank(message = "lastName cannot be blank")
     private String lastName;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
     private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
     private String password;
 
 }
