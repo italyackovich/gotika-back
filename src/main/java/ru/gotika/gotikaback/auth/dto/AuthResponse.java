46d4240b89503109851088e2f707983562256fc8
Ilya <pechenka.ivk3@gmail.com>
2025-02-03 19:57:52 +0400

    feat: added validation for AuthResponse class

--- src/main/java/ru/gotika/gotikaback/auth/dto/AuthResponse.java
+++ src/main/java/ru/gotika/gotikaback/auth/dto/AuthResponse.java
@@ -1,5 +1,7 @@
 package ru.gotika.gotikaback.auth.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import ru.gotika.gotikaback.user.dto.UserDto;
@@ -8,6 +10,11 @@ import ru.gotika.gotikaback.user.dto.UserDto;
 @AllArgsConstructor
 public class AuthResponse {
 
+    @NotNull(message = "CookieList is required")
+    @Valid
     private AccessRefreshCookies cookieList;
+
+    @NotNull(message = "UserDto is required")
+    @Valid
     private UserDto userDto;
 }
