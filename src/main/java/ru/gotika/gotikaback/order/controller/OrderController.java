4c1355aaf378df72e3e5b522b5e3d6d5317097e4
Ilya <pechenka.ivk3@gmail.com>
2025-02-04 11:23:42 +0400

    chore: extracted getByteArrayOutputStream method from generateOrderReport

--- src/main/java/ru/gotika/gotikaback/order/controller/OrderController.java
+++ src/main/java/ru/gotika/gotikaback/order/controller/OrderController.java
@@ -36,16 +36,7 @@ public class OrderController {
     public ResponseEntity<?> generateOrderReport(@RequestParam Long restaurantId) throws IOException {
         List<Order> orders = orderService.getOrdersForLastMonth(restaurantId);
 
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream))) {
-            writer.write("Отчет заказов за последний месяц\n\n");
-            for (Order order : orders) {
-                writer.write("ID заказа: " + order.getId() + "\n");
-                writer.write("Имя клиента: " + order.getUser().getFirstName() + " " + order.getUser().getLastName() + "\n");
-                writer.write("Сумма заказа: " + order.getTotalAmount() + "\n");
-                writer.write("Дата заказа: " + order.getOrderDate() + "\n\n");
-            }
-        }
+        ByteArrayOutputStream outputStream = getByteArrayOutputStream(orders);
 
         // Чтение файла в поток
         InputStreamResource resource = new InputStreamResource(new ByteArrayInputStream(outputStream.toByteArray()));
@@ -80,5 +71,19 @@ public class OrderController {
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         orderService.deleteOrder(id);
         return ResponseEntity.noContent().build();
+    }
+
+    private static ByteArrayOutputStream getByteArrayOutputStream(List<Order> orders) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream))) {
+            writer.write("Отчет заказов за последний месяц\n\n");
+            for (Order order : orders) {
+                writer.write("ID заказа: " + order.getId() + "\n");
+                writer.write("Имя клиента: " + order.getUser().getFirstName() + " " + order.getUser().getLastName() + "\n");
+                writer.write("Сумма заказа: " + order.getTotalAmount() + "\n");
+                writer.write("Дата заказа: " + order.getOrderDate() + "\n\n");
+            }
+        }
+        return outputStream;
     }
 }
