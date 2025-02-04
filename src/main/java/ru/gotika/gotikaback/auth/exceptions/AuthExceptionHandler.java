9436a16769c0154117fd1602bc70a46d9214f611
Ilya <pechenka.ivk3@gmail.com>
2025-02-04 10:20:33 +0400

    refactor: change import package HttpStatus from org.apache.hc.core5.http to org.springframework.http

--- src/main/java/ru/gotika/gotikaback/auth/exceptions/AuthExceptionHandler.java
+++ src/main/java/ru/gotika/gotikaback/auth/exceptions/AuthExceptionHandler.java
@@ -1,37 +1,39 @@
 package ru.gotika.gotikaback.auth.exceptions;
 
 import io.jsonwebtoken.ExpiredJwtException;
-import org.apache.hc.core5.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class AuthExceptionHandler {
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<String> handleExpiredJwtException(ExpiredJwtException e) {
-        return ResponseEntity.status(HttpStatus.SC_UNAUTHORIZED).body("JWT expired: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("JWT expired: " + e.getMessage());
     }
 
     @ExceptionHandler(MissingCookieException.class)
     public ResponseEntity<String> handleMissingCookieException(MissingCookieException e) {
-        return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body("Missing cookie: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing cookie: " + e.getMessage());
     }
 
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<String> handleInvalidTokenException(InvalidTokenException e) {
-        return ResponseEntity.status(HttpStatus.SC_BAD_REQUEST).body("Invalid token: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid token: " + e.getMessage());
     }
 
     @ExceptionHandler(TokenNotFoundException.class)
     public ResponseEntity<String> handleTokenNotFoundException(TokenNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.SC_NOT_FOUND).body("Token not found: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Token not found: " + e.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
-        return ResponseEntity.status(HttpStatus.SC_UNAUTHORIZED).body("Unauthorized: " + e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized: " + e.getMessage());
     }
 }
