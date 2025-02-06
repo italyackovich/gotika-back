850d8b46192202cd8938696f61cdfd24f3817139
Ilya <pechenka.ivk3@gmail.com>
2025-02-06 18:32:27 +0400

    refactor: added implementing Serializable interface and new constructor with Throwable cause for UserNotFoundException

--- src/main/java/ru/gotika/gotikaback/user/exception/UserNotFoundException.java
+++ src/main/java/ru/gotika/gotikaback/user/exception/UserNotFoundException.java
@@ -1,7 +1,17 @@
 package ru.gotika.gotikaback.user.exception;
 
-public class UserNotFoundException extends RuntimeException {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class UserNotFoundException extends RuntimeException implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     public UserNotFoundException(final String message) {
         super(message);
+    }
+
+    public UserNotFoundException(final String message, final Throwable cause) {
+        super(message, cause);
     }
 }
