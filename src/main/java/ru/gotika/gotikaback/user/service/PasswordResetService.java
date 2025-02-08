3cb1a30c5d709aea948dc777d4a0e356ccaa964a
Ilya <pechenka.ivk3@gmail.com>
2025-02-08 19:49:07 +0400

    docs: add Javadoc for user module services - add Javadoc for PasswordResetService - add Javadoc for UserService

--- src/main/java/ru/gotika/gotikaback/user/service/PasswordResetService.java
+++ src/main/java/ru/gotika/gotikaback/user/service/PasswordResetService.java
@@ -6,8 +6,41 @@ import ru.gotika.gotikaback.user.dto.ConfirmResetPassword;
 import ru.gotika.gotikaback.user.dto.RequestResetPassword;
 
 public interface PasswordResetService {
-
+    /**
+     * Sends reset code to the user's email provided on the {@link RequestResetPassword}.
+     *
+     * @param requestResetPassword containing user's email.
+     *                             Must not be a null.
+     * @return a {@link ChangePasswordResponse} object that contains:
+     * <ul>
+     *     <li>{@code message}: a human-readable message describing the result of the operation.</li>
+     *     <li>{@code success}: a boolean flag indicating whether the operation was successful.</li>
+     * </ul>
+     */
     ChangePasswordResponse sendResetCode(RequestResetPassword requestResetPassword);
+
+    /**
+     * Checks the password reset code sent to the user's email provided on the {@link ConfirmResetPassword}.
+     *
+     * @param confirmResetPassword containing user's email and password reset code.
+     *                             Must not be a null.
+     * @return a {@link ChangePasswordResponse} object that contains:
+     * <ul>
+     *     <li>{@code message}: a human-readable message describing the result of the operation.</li>
+     *     <li>{@code success}: a boolean flag indicating whether the operation was successful.</li>
+     * </ul>
+     */
     ChangePasswordResponse validateResetCode(ConfirmResetPassword confirmResetPassword);
+
+    /**
+     * Changes user's password to new password provided on the {@link ChangePassword}.
+     *
+     * @param changePassword containing user's email and new password.
+     * @return a {@link ChangePasswordResponse} object that contains:
+     * <ul>
+     *     <li>{@code message}: a human-readable message describing the result of the operation.</li>
+     *     <li>{@code success}: a boolean flag indicating whether the operation was successful.</li>
+     * </ul>
+     */
     ChangePasswordResponse resetPassword(ChangePassword changePassword);
 }
--- src/main/java/ru/gotika/gotikaback/user/service/UserService.java
+++ src/main/java/ru/gotika/gotikaback/user/service/UserService.java
@@ -9,13 +9,90 @@ import ru.gotika.gotikaback.user.dto.UserDto;
 import java.util.List;
 
 public interface UserService {
+    /**
+     * Gets all users from database.
+     *
+     * @return a {@code List}<{@link UserDto}> containing list all users.
+     */
     List<UserDto> getAllUsers();
+
+    /**
+     * Gets user from database by {@code id}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     * @return an {@link UserDto} containing user's data.
+     */
     UserDto getUser(Long id);
+
+    /**
+     * Creates a new user based on the provided {@link UserDto}.
+     *
+     * @param userDto containing user's data.
+     *                Must not be a null.
+     * @return an {@link UserDto} containing created user's data.
+     */
     UserDto createUser(UserDto userDto);
+
+    /**
+     * Updates user by {@code id} and {@link UserDto}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     * @param userDto containing user's data.
+     *                Must not be a null.
+     * @return an {@link UserDto} containing updated user's data.
+     */
     UserDto updateUser(Long id, UserDto userDto);
+
+    /**
+     * Changes user's address by {@code id} and {@link ChangeAddress}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     * @param changeAddress containing new user's address.
+     *                      Must not be a null.
+     * @return an {@link UserDto} containing user's data with updated address.
+     */
     UserDto changeUserAddress(Long id, ChangeAddress changeAddress);
+
+    /**
+     * Changes user's role by {@code id} and {@link ChangeRoleDto}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     * @param changeRoleDto containing new user's role.
+     *                      Must not be a null.
+     * @return an {@link UserDto} containing user's data with updated role.
+     */
     UserDto changeRole(Long id, ChangeRoleDto changeRoleDto);
+
+    /**
+     * Changes user's image by {@code id} and {@link MultipartFile}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     * @param image containing new user's image.
+     * @return an {@link UserDto} containing user's data with updated image.
+     */
     UserDto changeImage(Long id, MultipartFile image);
+
+    /**
+     * Changes user's credentials by {@code id} and {@link ChangeUserCredentials}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     * @param userCredentials containing new user's credentials.
+     *                        Must not be a null.
+     * @return an {@link UserDto} containing user's data with updated credentials.
+     */
     UserDto changeUserCred(Long id, ChangeUserCredentials userCredentials);
+
+    /**
+     * Deletes user by {@code id}.
+     *
+     * @param id containing user's id.
+     *           Must not be a null.
+     */
     void deleteUser(Long id);
 }
