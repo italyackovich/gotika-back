7a685133f4a7cf9ee310c682da74440daad72a99
Ilya <pechenka.ivk3@gmail.com>
2025-02-09 19:16:10 +0400

    refactor: add logging and exception for ReviewServiceImpl - add throw ReviewNotFoundException - add logging for all methods

--- src/main/java/ru/gotika/gotikaback/review/service/impl/ReviewServiceImpl.java
+++ src/main/java/ru/gotika/gotikaback/review/service/impl/ReviewServiceImpl.java
@@ -1,11 +1,13 @@
 package ru.gotika.gotikaback.review.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.gotika.gotikaback.restaurant.model.Restaurant;
 import ru.gotika.gotikaback.restaurant.repository.RestaurantRepository;
 import ru.gotika.gotikaback.review.dto.ReviewDto;
+import ru.gotika.gotikaback.review.exception.ReviewNotFoundException;
 import ru.gotika.gotikaback.review.mapper.ReviewMapper;
 import ru.gotika.gotikaback.review.model.Review;
 import ru.gotika.gotikaback.review.repository.ReviewRepository;
@@ -17,6 +19,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ReviewServiceImpl implements ReviewService {
 
     private final RestaurantRepository restaurantRepository;
@@ -25,29 +28,27 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Override
     public List<ReviewDto> getAllReviews() {
+        log.info("Get all reviews");
         return reviewMapper.reviewListToReviewDtoList(reviewRepository.findAll());
     }
 
     @Override
     public ReviewDto getReviewById(Long id) {
-        return reviewMapper.reviewToReviewDto(reviewRepository.findById(id).orElse(null));
+        log.info("Get review with id {}", id);
+        return reviewMapper.reviewToReviewDto(reviewRepository.findById(id)
+                .orElseThrow(() -> new ReviewNotFoundException("Review with id " + id + " not found")));
     }
 
     @Transactional
     @Override
     public ReviewDto createReview(ReviewDto reviewDto) {
 
-        if (reviewDto.getRating() < 1 || reviewDto.getRating() > 5) {
-            throw new IllegalArgumentException("Rating must be between 1 and 5");
-        }
-
         Review review = reviewMapper.reviewDtoToReview(reviewDto);
         review.setCreatedAt(LocalDateTime.now());
-
         Review savedReview = reviewRepository.save(review);
 
         updateAverageRating(savedReview.getRestaurant().getId());
-
+        log.info("Review created: {}", savedReview);
         return reviewMapper.reviewToReviewDto(reviewRepository.save(review));
     }
 
@@ -59,37 +60,36 @@ public class ReviewServiceImpl implements ReviewService {
             Restaurant restaurant = restaurantOptional.get();
             List<Review> reviews = restaurant.getReviewList();
 
-            int sum = reviews.stream()
-                    .map(Review::getRating)
-                    .reduce(0, Integer::sum);
-
-            float averageRating = reviews.isEmpty() ? 0 : (float) sum / reviews.size();
+            float averageRating = (float) reviews.stream()
+                    .mapToInt(Review::getRating)
+                    .average()
+                    .orElse(0f);
 
             averageRating = Math.max(1, Math.min(averageRating, 5));
 
             restaurant.setRating(averageRating);
             restaurantRepository.save(restaurant);
+            log.info("Review updated averageRating: {} for restaurant with id {}", averageRating, restaurantId);
         }
+
     }
 
     @Transactional
     @Override
     public ReviewDto updateReview(Long id, ReviewDto reviewDto) {
-        if (reviewDto.getRating() != null && (reviewDto.getRating() < 1 || reviewDto.getRating() > 5)) {
-            throw new IllegalArgumentException("Rating must be between 1 and 5");
-        }
-
         return reviewRepository.findById(id).map(review -> {
             Review updatedReview = reviewMapper.reviewDtoToReview(reviewDto);
             updatedReview.setId(review.getId());
             reviewRepository.save(updatedReview);
             updateAverageRating(updatedReview.getRestaurant().getId());
+            log.info("Review with id {} updated: {}", id, updatedReview);
             return reviewMapper.reviewToReviewDto(updatedReview);
-        }).orElse(null);
+        }).orElseThrow(() -> new ReviewNotFoundException("Review with id " + id + " not found"));
     }
 
     @Override
     public void deleteReview(Long id) {
         reviewRepository.deleteById(id);
+        log.info("Review with id {} deleted", id);
     }
 }
