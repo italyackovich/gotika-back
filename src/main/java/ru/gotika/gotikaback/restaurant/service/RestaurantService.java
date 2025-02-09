fbf8461417362896498537cfb9f86b40092bf247
Ilya <pechenka.ivk3@gmail.com>
2025-02-09 19:55:37 +0400

    refactor: add exception and logging - add throw RestaurantNotFoundException - add logging for all methods

--- src/main/java/ru/gotika/gotikaback/restaurant/service/RestaurantService.java
+++ src/main/java/ru/gotika/gotikaback/restaurant/service/RestaurantService.java
@@ -1,7 +1,7 @@
 package ru.gotika.gotikaback.restaurant.service;
 
 import org.springframework.web.multipart.MultipartFile;
-import ru.gotika.gotikaback.restaurant.dto.ChangeRequest;
+import ru.gotika.gotikaback.restaurant.dto.ChangeRestaurantCred;
 import ru.gotika.gotikaback.restaurant.dto.RestaurantDto;
 
 import java.util.List;
@@ -10,7 +10,7 @@ public interface RestaurantService {
     List<RestaurantDto> getRestaurants();
     RestaurantDto getRestaurant(Long id);
     RestaurantDto createRestaurant(RestaurantDto restaurantDto);
-    RestaurantDto patchRestaurant(Long id, ChangeRequest changeRequest);
+    RestaurantDto changeCred(Long id, ChangeRestaurantCred changeRequest);
     RestaurantDto updateRestaurant(Long id, RestaurantDto restaurantDto);
     RestaurantDto changeImage(Long id, MultipartFile file);
     void deleteRestaurant(Long id);
--- src/main/java/ru/gotika/gotikaback/restaurant/service/impl/RestaurantServiceImpl.java
+++ src/main/java/ru/gotika/gotikaback/restaurant/service/impl/RestaurantServiceImpl.java
@@ -1,11 +1,13 @@
 package ru.gotika.gotikaback.restaurant.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.gotika.gotikaback.common.service.CloudinaryService;
-import ru.gotika.gotikaback.restaurant.dto.ChangeRequest;
+import ru.gotika.gotikaback.restaurant.dto.ChangeRestaurantCred;
 import ru.gotika.gotikaback.restaurant.dto.RestaurantDto;
+import ru.gotika.gotikaback.restaurant.exception.RestaurantNotFoundException;
 import ru.gotika.gotikaback.restaurant.mapper.RestaurantMapper;
 import ru.gotika.gotikaback.restaurant.model.Restaurant;
 import ru.gotika.gotikaback.restaurant.repository.RestaurantRepository;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RestaurantServiceImpl implements RestaurantService {
 
     private final RestaurantRepository restaurantRepository;
@@ -23,30 +26,35 @@ public class RestaurantServiceImpl implements RestaurantService {
 
     @Override
     public List<RestaurantDto> getRestaurants() {
+        log.info("Get all restaurants");
         return restaurantMapper.restaurantListToRestaurantDtoList(restaurantRepository.findAll());
     }
 
     @Override
     public RestaurantDto getRestaurant(Long id) {
-        return restaurantMapper.restaurantToRestaurantDto(restaurantRepository.findById(id).orElse(null));
+        log.info("Get restaurant with id: {}", id);
+        return restaurantMapper.restaurantToRestaurantDto(restaurantRepository.findById(id)
+                .orElseThrow(() -> new RestaurantNotFoundException("Restaurant with id: " + id + " not found")));
     }
 
     @Override
     public RestaurantDto createRestaurant(RestaurantDto restaurantDto) {
-        restaurantRepository.save(restaurantMapper.restaurantDtoToRestaurant(restaurantDto));
-        return restaurantDto;
+        Restaurant restaurant = restaurantRepository.save(restaurantMapper.restaurantDtoToRestaurant(restaurantDto));
+        log.info("Created restaurant: {}", restaurant);
+        return restaurantMapper.restaurantToRestaurantDto(restaurant);
     }
 
     @Override
-    public RestaurantDto patchRestaurant(Long id, ChangeRequest changeRequest) {
+    public RestaurantDto changeCred(Long id, ChangeRestaurantCred changeRequest) {
         return restaurantRepository.findById(id).map(restaurant -> {
             restaurant.setName(changeRequest.getName());
             restaurant.setAddress(changeRequest.getAddress());
             restaurant.setOpeningHours(changeRequest.getOpeningHours());
             restaurant.setPhoneNumber(changeRequest.getPhoneNumber());
             restaurantRepository.save(restaurant);
+            log.info("Changed restaurant credentials: {}", restaurant);
             return restaurantMapper.restaurantToRestaurantDto(restaurant);
-        }).orElseThrow(() -> new RuntimeException("Restaurant not found"));
+        }).orElseThrow(() -> new RestaurantNotFoundException("Restaurant with id " + id + " not found"));
     }
 
     @Override
@@ -55,21 +63,24 @@ public class RestaurantServiceImpl implements RestaurantService {
             Restaurant updatedRestaurant = restaurantMapper.restaurantDtoToRestaurant(restaurantDto);
             updatedRestaurant.setId(id);
             restaurantRepository.save(updatedRestaurant);
+            log.info("Updated restaurant: {}", updatedRestaurant);
             return restaurantMapper.restaurantToRestaurantDto(updatedRestaurant);
-        }).orElseThrow(() -> new RuntimeException("Restaurant not found"));
+        }).orElseThrow(() -> new RestaurantNotFoundException("Restaurant with id: " + id + " not found"));
     }
 
     @Override
     public RestaurantDto changeImage(Long id, MultipartFile file) {
-        return restaurantRepository.findById(id).map(r -> {
-            r.setImageUrl(cloudinaryService.uploadFile(file));
-            restaurantRepository.save(r);
-            return restaurantMapper.restaurantToRestaurantDto(r);
-        }).orElseThrow(null);
+        return restaurantRepository.findById(id).map(restaurant -> {
+            restaurant.setImageUrl(cloudinaryService.uploadFile(file));
+            restaurantRepository.save(restaurant);
+            log.info("Changed restaurant image: {}", restaurant);
+            return restaurantMapper.restaurantToRestaurantDto(restaurant);
+        }).orElseThrow(() -> new RestaurantNotFoundException("Restaurant with id: " + id + " not found"));
     }
 
     @Override
     public void deleteRestaurant(Long id) {
         restaurantRepository.deleteById(id);
+        log.info("Restaurant with id: {} deleted", id);
     }
 }
