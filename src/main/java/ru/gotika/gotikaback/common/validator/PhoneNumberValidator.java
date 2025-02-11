087fb7c6011707ee2700172e8b19cd4ae84b9268
Ilya <pechenka.ivk3@gmail.com>
2025-02-11 21:40:20 +0400

    fix(common): fix isValid method of PhoneNumberValidator class

--- src/main/java/ru/gotika/gotikaback/common/validator/PhoneNumberValidator.java
+++ src/main/java/ru/gotika/gotikaback/common/validator/PhoneNumberValidator.java
@@ -7,7 +7,7 @@ import ru.gotika.gotikaback.common.annotation.ValidPhoneNumber;
 public class PhoneNumberValidator implements ConstraintValidator<ValidPhoneNumber, String> {
     @Override
     public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
-        if(phoneNumber == null) return false;
+        if(phoneNumber == null) return true;
         return phoneNumber.matches("^(\\+7|8)[-\\s]?\\(?[0-9]{3}\\)?[-\\s]?[0-9]{3}[-\\s]?[0-9]{2}[-\\s]?[0-9]{2}$");
     }
 }
