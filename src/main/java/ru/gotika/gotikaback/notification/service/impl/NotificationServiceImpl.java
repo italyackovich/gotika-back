9cf08c239034d132533706aaaa658f820f4e3526
Ilya <pechenka.ivk3@gmail.com>
2025-02-25 18:44:54 +0400

    chore(notification): add logging in NotificationServiceImpl

--- src/main/java/ru/gotika/gotikaback/notification/service/impl/NotificationServiceImpl.java
+++ src/main/java/ru/gotika/gotikaback/notification/service/impl/NotificationServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.gotika.gotikaback.notification.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.gotika.gotikaback.notification.dto.NotificationDto;
 import ru.gotika.gotikaback.notification.exception.NotificationNotFoundException;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationServiceImpl implements NotificationService {
 
     private final NotificationRepository notificationRepository;
@@ -21,23 +23,27 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public List<NotificationDto> getAllNotifications() {
+        log.info("Get all notifications");
         return notificationMapper.notificationListToNotificationDtoList(notificationRepository.findAll());
     }
 
     @Override
     public NotificationDto getNotificationById(Long id) {
+        log.info("Get notification by id: {}", id);
         return notificationMapper.notificationToNotificationDto(notificationRepository.findById(id)
                 .orElseThrow(() -> new NotificationNotFoundException("Notification by id: " + id + " not found")));
     }
 
     @Override
     public NotificationDto createNotification(NotificationDto notificationDto) {
+        log.info("Create notification: {}", notificationDto);
         Notification notification = notificationRepository.save(notificationMapper.notificationDtoToNotification(notificationDto));
         return notificationMapper.notificationToNotificationDto(notification);
     }
 
     @Override
     public NotificationDto updateNotification(Long id, NotificationDto notificationDto) {
+        log.info("Update notification: {}", notificationDto);
         return notificationRepository.findById(id).map(notification -> {
             Notification updatedNotification = notificationMapper.notificationDtoToNotification(notificationDto);
             updatedNotification.setId(notification.getId());
@@ -48,6 +54,7 @@ public class NotificationServiceImpl implements NotificationService {
 
     @Override
     public void deleteNotification(Long id) {
+        log.info("Delete notification: {}", id);
         notificationRepository.deleteById(id);
     }
 }
