a68651f327442c113ca35144d22f41078e35de20
Ilya <pechenka.ivk3@gmail.com>
2025-02-26 20:09:19 +0400

    refactor(order): improve validation for order module - add positive or zero validation for quantity of OrderItemDto - add positive or zero validation for price of OrderItemDto - add positive or zero validation for order id of OrderItemDto - add positive or zero validation for dish id of OrderItemDto - add positive or zero validation for total amount of OrderDto - add positive or zero validation for user id of OrderDto - add positive or zero validation for restaurant id of OrderDto

--- src/main/java/ru/gotika/gotikaback/order/dto/OrderDto.java
+++ src/main/java/ru/gotika/gotikaback/order/dto/OrderDto.java
@@ -1,6 +1,7 @@
 package ru.gotika.gotikaback.order.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,15 +18,18 @@ public class OrderDto {
     private Long id;
 
     @NotNull(message = "Total amount cannot be a null")
+    @PositiveOrZero(message = "Total amount must be at least 0")
     private Double totalAmount;
 
     private LocalTime deliveryTime;
     private List<OrderItemDto> orderItems;
 
     @NotNull(message = "User id cannot be a null")
+    @PositiveOrZero(message = "User id must be at least 0")
     private Long userId;
 
     @NotNull(message = "Restaurant id cannot be a null")
+    @PositiveOrZero(message = "Restaurant id must be at least 0")
     private Long restaurantId;
     private RequestPaymentDto payment;
 
--- src/main/java/ru/gotika/gotikaback/order/dto/OrderItemDto.java
+++ src/main/java/ru/gotika/gotikaback/order/dto/OrderItemDto.java
@@ -1,6 +1,7 @@
 package ru.gotika.gotikaback.order.dto;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,15 +15,19 @@ public class OrderItemDto {
     private Long id;
 
     @NotNull(message = "Quantity cannot be a null")
+    @PositiveOrZero(message = "Quantity must be at least 0")
     private Integer quantity;
 
     @NotNull(message = "Price cannot be a null")
+    @PositiveOrZero(message = "Price id must be at least 0")
     private Double price;
 
     @NotNull(message = "Order id cannot be a null")
+    @PositiveOrZero(message = "Order id must be at least 0")
     private Long orderId;
 
     @NotNull(message = "Dish id cannot be a null")
+    @PositiveOrZero(message = "Dish id must be at least 0")
     private Long dishId;
     private DishDto dish;
 
