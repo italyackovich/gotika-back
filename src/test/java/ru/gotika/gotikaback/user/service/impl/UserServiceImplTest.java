847c8a99cbe3280c70c4ba1597364c7a81d941c5
Ilya <pechenka.ivk3@gmail.com>
2025-03-03 19:35:47 +0400

    test(user): improve UserServiceImplTest - add changeAddress_ShouldUpdateUserAddress_IfUserExisting method - add changeAddress_ShouldThrowException_IfUserNotFound method - add changeRole_ShouldUpdateUserRole_IfUserExisting method - add changeRole_ShouldThrowException_IfUserNotFound method - add changeImage_ShouldUpdateUserImage_IfUserExisting method - add changeImage_ShouldThrowException_IfUserNotFound method - add changeCred_ShouldUpdateUserCred_IfUserExisting method - add changeCred_ShouldThrowException_IfUserNotFound method

--- src/test/java/ru/gotika/gotikaback/user/service/impl/UserServiceImplTest.java
+++ src/test/java/ru/gotika/gotikaback/user/service/impl/UserServiceImplTest.java
@@ -6,8 +6,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.multipart.MultipartFile;
 import ru.gotika.gotikaback.common.service.CloudinaryService;
+import ru.gotika.gotikaback.user.dto.ChangeAddress;
+import ru.gotika.gotikaback.user.dto.ChangeRoleDto;
+import ru.gotika.gotikaback.user.dto.ChangeUserCredentials;
 import ru.gotika.gotikaback.user.dto.UserDto;
+import ru.gotika.gotikaback.user.enums.Role;
 import ru.gotika.gotikaback.user.exception.UserNotFoundException;
 import ru.gotika.gotikaback.user.mapper.UserMapper;
 import ru.gotika.gotikaback.user.model.User;
@@ -36,7 +41,7 @@ public class UserServiceImplTest {
     private UserServiceImpl userServiceImpl;
 
     @Test
-    void getALlUsers_ShouldReturnListOfUserDto() {
+    void getAllUsers_ShouldReturnListOfUserDto() {
         List<User> userList = Arrays.asList(new User(), new User());
         when(userRepository.findAll()).thenReturn(userList);
 
@@ -158,6 +163,192 @@ public class UserServiceImplTest {
 
         assertThrows(UserNotFoundException.class, () -> userServiceImpl.updateUser(userId, updateDto));
 
+        verify(userRepository).findById(userId);
+        verifyNoMoreInteractions(userMapper);
+    }
+
+    @Test
+    void changeAddress_ShouldUpdateUserAddress_IfUserExisting() {
+        Long userId = 1L;
+        User user = new User();
+        user.setId(userId);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        ChangeAddress changeAddress = new ChangeAddress();
+        changeAddress.setAddress("newAddress");
+        user.setAddress(changeAddress.getAddress());
+
+        User savedUser = new User();
+        savedUser.setId(userId);
+        savedUser.setAddress("newAddress");
+        when(userRepository.save(user)).thenReturn(savedUser);
+
+        UserDto savedDto = new UserDto();
+        savedDto.setId(userId);
+        savedDto.setAddress("newAddress");
+        when(userMapper.userToUserDto(savedUser)).thenReturn(savedDto);
+
+        UserDto result = userServiceImpl.changeAddress(userId, changeAddress);
+
+        assertEquals(userId, result.getId());
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(user);
+        verify(userMapper).userToUserDto(savedUser);
+    }
+
+    @Test
+    void changeAddress_ShouldThrowException_IfUserNotFound() {
+        Long userId = 999L;
+        ChangeAddress changeAddress = new ChangeAddress();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userServiceImpl.changeAddress(userId, changeAddress));
+
+        verify(userRepository).findById(userId);
+        verifyNoMoreInteractions(userMapper);
+    }
+
+    @Test
+    void changeRole_ShouldUpdateUserRole_IfUserExisting() {
+        Long userId = 1L;
+        User user = new User();
+        user.setId(userId);
+        user.setRole(Role.ROLE_CLIENT);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        ChangeRoleDto changeRoleDto = new ChangeRoleDto();
+        changeRoleDto.setRole(Role.ROLE_ADMIN);
+        user.setRole(changeRoleDto.getRole());
+
+        User savedUser = new User();
+        savedUser.setId(userId);
+        savedUser.setRole(Role.ROLE_ADMIN);
+
+        when(userRepository.save(user)).thenReturn(savedUser);
+
+        UserDto savedDto = new UserDto();
+        savedDto.setId(userId);
+        savedDto.setRole(Role.ROLE_ADMIN);
+
+        when(userMapper.userToUserDto(savedUser)).thenReturn(savedDto);
+
+        UserDto result = userServiceImpl.changeRole(userId, changeRoleDto);
+
+        assertEquals(userId, result.getId());
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(user);
+        verify(userMapper).userToUserDto(savedUser);
+    }
+
+    @Test
+    void changeRole_ShouldThrowException_IfUserNotFound() {
+        Long userId = 999L;
+        ChangeRoleDto changeRoleDto = new ChangeRoleDto();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userServiceImpl.changeRole(userId, changeRoleDto));
+
+        verify(userRepository).findById(userId);
+        verifyNoMoreInteractions(userMapper);
+    }
+
+    @Test
+    void changeImage_ShouldUpdateUserImage_IfUserExisting() {
+        Long userId = 1L;
+        MultipartFile image = mock(MultipartFile.class);
+
+        User user = new User();
+        user.setId(userId);
+        user.setImageUrl("oldImageUrl");
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        String newImageUrl = "newImageUrl";
+
+        when(cloudinaryService.uploadFile(image)).thenReturn(newImageUrl);
+        when(userRepository.save(user)).thenReturn(user);
+
+        UserDto expectedDto = new UserDto();
+        expectedDto.setId(userId);
+
+        when(userMapper.userToUserDto(user)).thenReturn(expectedDto);
+
+        UserDto result = userServiceImpl.changeImage(userId, image);
+
+        assertEquals(expectedDto, result);
+        verify(userRepository).findById(userId);
+        verify(cloudinaryService).uploadFile(image);
+        verify(userRepository).save(user);
+        verify(userMapper).userToUserDto(user);
+    }
+
+    @Test
+    void changeImage_ShouldThrowException_IfUserNotFound() {
+        Long userId = 999L;
+        User user = new User();
+        user.setId(userId);
+        MultipartFile image = mock(MultipartFile.class);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userServiceImpl.changeImage(userId, image));
+        verify(userRepository).findById(userId);
+        verifyNoMoreInteractions(userMapper);
+    }
+
+    @Test
+    void changeCred_ShouldUpdateUserCred_IfUserExisting() {
+        Long userId = 1L;
+        User user = new User();
+        user.setId(userId);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        ChangeUserCredentials changeUserCredentials = new ChangeUserCredentials();
+        changeUserCredentials.setFirstName("newFirstName");
+        changeUserCredentials.setPhoneNumber("newPhoneNumber");
+        changeUserCredentials.setLastName("newLastName");
+        user.setFirstName(changeUserCredentials.getFirstName());
+        user.setPhoneNumber(changeUserCredentials.getPhoneNumber());
+        user.setLastName(changeUserCredentials.getLastName());
+
+        User savedUser = new User();
+        savedUser.setId(userId);
+        savedUser.setFirstName("newFirstName");
+        savedUser.setPhoneNumber("newPhoneNumber");
+        savedUser.setLastName("newLastName");
+
+        when(userRepository.save(user)).thenReturn(savedUser);
+
+        UserDto userDto = new UserDto();
+        userDto.setId(userId);
+        userDto.setFirstName("newFirstName");
+        userDto.setPhoneNumber("newPhoneNumber");
+        userDto.setLastName("newLastName");
+        when(userMapper.userToUserDto(savedUser)).thenReturn(userDto);
+
+        UserDto result = userServiceImpl.changeCred(userId, changeUserCredentials);
+
+        assertEquals(userId, result.getId());
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(user);
+        verify(userMapper).userToUserDto(savedUser);
+    }
+
+    @Test
+    void changeCred_ShouldThrowException_IfUserNotFound() {
+        Long userId = 999L;
+        User user = new User();
+        user.setId(userId);
+        ChangeUserCredentials userCredentials = new ChangeUserCredentials();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        assertThrows(UserNotFoundException.class, () -> userServiceImpl.changeCred(userId, userCredentials));
         verify(userRepository).findById(userId);
         verifyNoMoreInteractions(userMapper);
     }
