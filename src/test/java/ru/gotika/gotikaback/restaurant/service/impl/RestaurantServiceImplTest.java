2e7db993a75b0f1515df78a5b76de15b25dc7184
Ilya <pechenka.ivk3@gmail.com>
2025-03-11 21:46:13 +0400

    test(restaurant): create RestaurantServiceImplTest - add getRestaurants_ShouldReturnListOfResponseRestaurantDto method - add getRestaurantById_ShouldReturnResponseRestaurantDto method - add getRestaurantById_ShouldThrowRestaurantNotFoundException_WhenRestaurantDoesNotExist method

---
+++ src/test/java/ru/gotika/gotikaback/restaurant/service/impl/RestaurantServiceImplTest.java
@@ -0,0 +1,104 @@
+package ru.gotika.gotikaback.restaurant.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.gotika.gotikaback.common.service.CloudinaryService;
+import ru.gotika.gotikaback.restaurant.dto.ResponseRestaurantDto;
+import ru.gotika.gotikaback.restaurant.exception.RestaurantNotFoundException;
+import ru.gotika.gotikaback.restaurant.mapper.RestaurantMapper;
+import ru.gotika.gotikaback.restaurant.model.Restaurant;
+import ru.gotika.gotikaback.restaurant.repository.RestaurantRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class RestaurantServiceImplTest {
+
+    @Mock
+    private RestaurantRepository restaurantRepository;
+
+    @Mock
+    private RestaurantMapper restaurantMapper;
+
+    @Mock
+    private CloudinaryService cloudinaryService;
+
+    @InjectMocks
+    private RestaurantServiceImpl restaurantService;
+
+    @Test
+    void getRestaurants_ShouldReturnListOfResponseRestaurantDto() {
+        Restaurant restaurant1 = new Restaurant();
+        restaurant1.setId(1L); // уникальное значение для restaurant1
+
+        Restaurant restaurant2 = new Restaurant();
+        restaurant2.setId(2L); // уникальное значение для restaurant2
+
+        List<Restaurant> restaurantList = List.of(restaurant1, restaurant2);
+
+        when(restaurantRepository.findAll()).thenReturn(restaurantList);
+
+        ResponseRestaurantDto dto1 = new ResponseRestaurantDto();
+        dto1.setId(1L); // установите идентификатор или другое поле
+        ResponseRestaurantDto dto2 = new ResponseRestaurantDto();
+        dto2.setId(2L);
+
+        when(restaurantMapper.restaurantToResponseRestaurantDto(restaurant1)).thenReturn(dto1);
+        when(restaurantMapper.restaurantToResponseRestaurantDto(restaurant2)).thenReturn(dto2);
+
+        List<ResponseRestaurantDto> result = restaurantService.getRestaurants();
+
+        assertEquals(2, result.size());
+        assertEquals(dto1, result.get(0));
+        assertEquals(dto2, result.get(1));
+
+        verify(restaurantRepository).findAll();
+        verify(restaurantMapper).restaurantToResponseRestaurantDto(restaurant1);
+        verify(restaurantMapper).restaurantToResponseRestaurantDto(restaurant2);
+        verifyNoMoreInteractions(restaurantRepository, restaurantMapper);
+
+    }
+
+
+    @Test
+    void getRestaurantById_ShouldReturnResponseRestaurantDto_WhenRestaurantExists() {
+        Long restaurantId = 1L;
+        Restaurant restaurant1 = new Restaurant();
+        restaurant1.setId(restaurantId);
+
+        when(restaurantRepository.findById(restaurantId)).thenReturn(Optional.of(restaurant1));
+
+        when(restaurantMapper.restaurantToResponseRestaurantDto(restaurant1)).thenAnswer(invocation -> {
+            Restaurant restaurant = invocation.getArgument(0);
+            ResponseRestaurantDto responseRestaurantDto = new ResponseRestaurantDto();
+            responseRestaurantDto.setId(restaurant.getId());
+            return responseRestaurantDto;
+        });
+
+        ResponseRestaurantDto result = restaurantService.getRestaurant(restaurantId);
+
+        assertEquals(restaurantId, result.getId());
+        verify(restaurantRepository).findById(restaurantId);
+        verify(restaurantMapper).restaurantToResponseRestaurantDto(restaurant1);
+        verifyNoInteractions(cloudinaryService);
+    }
+
+    @Test
+    void getRestaurantById_ShouldThrowRestaurantNotFoundException_WhenRestaurantDoesNotExist() {
+        Long restaurantId = 999L;
+
+        when(restaurantRepository.findById(restaurantId)).thenReturn(Optional.empty());
+
+        assertThrows(RestaurantNotFoundException.class, () -> restaurantService.getRestaurant(restaurantId));
+
+        verify(restaurantRepository).findById(restaurantId);
+        verifyNoMoreInteractions(cloudinaryService, restaurantMapper);
+    }
+}
